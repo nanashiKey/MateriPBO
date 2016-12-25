c73b010a64d38eb4db42edb7d647f2ee4b63bbfd
Irfan Asidiq <Assidiq.irfan@gmail.com>
2016-12-25 15:22:32 +0700

    Create MembuatObjek.java
    beberapa contoh program sederhana Object Oriented Programming

---
+++ MembuatObjek.java
@@ -0,0 +1,11 @@
+public MembuatObjek{
+  public static void main(String [] args){
+    //cara mendeklarasi atau membuat sebuah objek dari class MembuatClass
+    MembuatClass kelasBaru = MembuatClass();
+    
+    //contoh penggunaan variable a yang ada pada class MembuatClass
+    //selanjutnya di inisiasi dengan memberikan nilai 15
+    kelasBaru.a = 15;
+    
+  }
+}
